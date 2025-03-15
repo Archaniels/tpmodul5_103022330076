382911069b171104250306b1c1f9704ef3319b2d
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-03-15 19:08:30 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ HaloGeneric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class HaloGeneric
+{
+    public HaloGeneric()
+    {
+    }
+
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,8 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Dani");
+    }
+}
